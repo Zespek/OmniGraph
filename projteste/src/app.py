bdb375a633ffa93d485cfb46879be32dea76ce5d
zespek <joezika2016@gmail.com>
2026-08-14 12:34:04 -0300

    chore: remove pasta de teste local do repositorio

--- projteste/src/app.py
+++
@@ -1,8 +0,0 @@
-from utils import saudar
-
-def main():
-    print(saudar("mundo"))
-
-if __name__ == "__main__":
-    main()
-# comentario
--- projteste/src/utils.py
+++
@@ -1,2 +0,0 @@
-def saudar(nome):
-    return f"Ola, {nome}"
